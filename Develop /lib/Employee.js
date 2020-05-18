e508774b2976d9a5b15326e78e1bf0e61583f4aa
Chris Rico <chris.r.rico@gmail.com>
2020-05-18 08:26:30 -0700

    creating repo

---
+++ Develop /lib/Employee.js
@@ -0,0 +1,13 @@
+// TODO: Write code to define and export the Employee class
+// construstor the name, age, school etc
+// write a property and method
+// protype the assisgnment 
+// push to array
+//run method
+//console log
+//append to html
+
+// * getName()
+// * getId()
+// * getEmail()
+// * getRole() // Returns 'Employee'
